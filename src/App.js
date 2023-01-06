301e12449419e420300dd0c983797e9eaa0029c9
Tejasp2003 <tejasp200320@gmail.com>
2023-01-06 22:33:51 +0530

    Created Header Componenet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Signup from './pages/Signup'
-import Signin from './pages/Signin'
-import ForgetPassword from './pages/ForgetPassword'
-import Offers from './pages/Offers'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import ForgetPassword from "./pages/ForgetPassword";
+import Offers from "./pages/Offers";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Profile" element={<Profile />} />
@@ -20,9 +23,8 @@ function App() {
           <Route path="/Offers" element={<Offers />} />
         </Routes>
       </Router>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+
+function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  function checkActiveRoute(route) {
+    if (location.pathname === route) {
+      return true;
+    }
+  }
+
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+          <img
+            src="/images/header-logo.svg"
+            alt="REALTOR"
+            className="h-5 cursor-pointer"
+            onClick={()=>{
+                navigate("/")
+            }}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10 ">
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent cursor-pointer ${
+                checkActiveRoute("/") && "text-black border-b-red-500"
+              }`}
+            >
+              <Link to={"/"}> Home </Link>
+            </li>
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[6px] border-b-transparent cursor-pointer ${
+                checkActiveRoute("/Offers") && "text-black border-b-red-500"
+              }`}
+            >
+              <Link to={"/Offers"}> Offers </Link>
+            </li>
+            <li
+              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent cursor-pointer ${
+                checkActiveRoute("/Signin") && "text-black border-b-red-500"
+              }`}
+            >
+              <Link to={"/Signin"}> Sign in </Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
