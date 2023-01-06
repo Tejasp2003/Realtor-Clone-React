e2f18fcf8f97aaf7621a3cddeb1a897412bc3719
Tejasp2003 <tejasp200320@gmail.com>
2023-01-06 20:15:24 +0530

    Created pages and Route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import ForgetPassword from './pages/ForgetPassword'
+import Offers from './pages/Offers'
 
 function App() {
   return (
-    <div className='text-2xl'>App</div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/ForgetPassword" element={<ForgetPassword />} />
+          <Route path="/Offers" element={<Offers />} />
+        </Routes>
+      </Router>
+
+    </>
   )
 }
 
